9d5cf77108ae2a0127ae98799f77307a3dfccbf7
irias <mrdidnew@gmail.com>
2025-09-02 09:21:02 +0300

    vector of numbers divisible by n^3

--- main.cpp
+++ main.cpp
@@ -1,10 +1,36 @@
 /// вектор конечный, и содержит числа, надо найти вектор кратных чисел числу n в кубе
 #include <iostream>
-
+#include <ostream>
+#include <string>
+#include <vector>
+#include <fstream>
 
 int main() {
 
-    std::cout << "hi" << std::endl;
-
+    int n;
+    std::cout << "input your n: ";
+    std::cin >> n;
+    std::fstream fd("numbers");
+    std::vector<int> rezlist;
+    if (fd.is_open()){
+        std::string tmp;
+        while (fd>>tmp){
+            int tmpnum = std::stoi(tmp);
+            if (tmpnum%(n*n*n) == 0){
+                rezlist.push_back(tmpnum);
+            };
+        }
+    } else {
+        std::cout << "File not found" << std::endl;
+        std::cerr << "File don`t open" << std::endl;
+    }
+    if (!rezlist.empty()){
+    std::cout << "The number increases by n³ : " << n*n*n << std::endl;
+    for (int i : rezlist){
+    std::cout << i << std::endl;
+    }
+    } else {
+        std::cout << "massive is empty" << std::endl;
+    }
     return 0;
 }
